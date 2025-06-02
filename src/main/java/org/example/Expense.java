2880b668f6ee63cbf4738d3f539718e1bcb72f68
arundaya <runsworkspace@gmail.com>
2025-06-02 23:04:33 +0700

    feat: combobox category otomatis menyesuaikan tipe transaksi, table jg akan terupdate ketika ada transaksi baru

--- src/main/java/org/example/Expense.java
+++ src/main/java/org/example/Expense.java
@@ -3,8 +3,9 @@ package org.example;
 import java.time.LocalDate;
 
 public class Expense extends Transaction {
-   public Expense(String txTitle, String txCategory, Double txNominal, LocalDate txTime) {
-      super(txTitle, txCategory, txNominal, txTime);
+   public Expense(String txTitle, String txCategory, Double txNominal, LocalDate txTime,
+         User user) {
+      super(txTitle, txCategory, txNominal, txTime, user);
       txType = "Expense";
    }
 }
--- src/main/java/org/example/Income.java
+++ src/main/java/org/example/Income.java
@@ -3,8 +3,8 @@ package org.example;
 import java.time.LocalDate;
 
 public class Income extends Transaction {
-   public Income(String txTitle, String txCategory, Double txNominal, LocalDate txTime) {
-      super(txTitle, txCategory, txNominal, txTime);
+   public Income(String txTitle, String txCategory, Double txNominal, LocalDate txTime, User user) {
+      super(txTitle, txCategory, txNominal, txTime, user);
       txType = "Income";
    }
 }
